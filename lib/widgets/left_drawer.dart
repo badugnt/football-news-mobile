af5066b787d8dc15f0cfbbcb0463423848011df6
edlyn <edlynmarva09@gmail.com>
2025-11-05 15:09:29 +0700

    Navigation, Layouts, Forms, and Input Elements

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:football_news/menu.dart';
+import 'package:football_news/screens/menu.dart';
 // TODO: Impor halaman NewsFormPage jika sudah dibuat
-import 'package:football_news/newslist_form.dart';
+import 'package:football_news/screens/newslist_form.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
