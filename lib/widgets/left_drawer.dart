e583da26bcfd2f8b80fdcfb5932fadcfdcc8a1e1
edlyn <edlynmarva09@gmail.com>
2025-11-12 18:13:11 +0700

    Flutter Networking, Authentication, and Integration

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 // TODO: Impor halaman NewsFormPage jika sudah dibuat
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -11,36 +12,34 @@ class LeftDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-             const DrawerHeader(
-            
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Column(
-                children: [
-                  Text(
-                    'Football News',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Column(
+              children: [
+                Text(
+                  'Football News',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  Padding(padding: EdgeInsets.all(10)),
-                  Text(
-                    "Seluruh berita sepak bola terkini di sini!",
-                    // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
-                    textAlign: TextAlign.center,
-                    style: TextStyle( 
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal,
-                    ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "Seluruh berita sepak bola terkini di sini!",
+                  // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-       
-        
+          ),
+
           // TODO: Bagian routing
           ListTile(
             leading: const Icon(Icons.home_outlined),
@@ -65,6 +64,19 @@ class LeftDrawer extends StatelessWidget {
               Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(builder: (context) => NewsFormPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.add_reaction_rounded),
+            title: const Text('News List'),
+            onTap: () {
+              // Route to news list page
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const NewsEntryListPage(),
+                ),
               );
             },
           ),
